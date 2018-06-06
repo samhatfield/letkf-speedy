c442b2387b6eee0428b72120fae08a4864b681c7
Sam Hatfield <samuel.hatfield@jesus.ox.ac.uk>
2018-06-06 17:37:33 +0100

    Updated experiment processing script

--- scripts/process.sh
+++ scripts/process.sh
@@ -8,10 +8,14 @@ function grd2nc {
 function fixmetadata {
     echo "Fixing NetCDF metadata for $1"
     ncatted -a standard_name,lev,c,c,atmosphere_sigma_coordinate $SPEEDY/experiments/$exp_name/$1.nc
+    ncatted -a long_name,lev,d,c, $SPEEDY/experiments/$exp_name/$1.nc
     ncatted -a units,lev,d,c, $SPEEDY/experiments/$exp_name/$1.nc
 }
 
 SPEEDY=$(dirname `pwd`)
+
+# Source experiment configuration
+source $SPEEDY/config.sh
 
 if [ $# -lt 2 ]; then
     echo "bash process.sh EXPERIMENT_NAME PRECISION"
@@ -45,10 +49,29 @@ if [ -d "$SPEEDY/DATA/nature_t39" ]; then
     fi
 fi
 
-# Process ensemble mean and spread
-grd2nc ensemble/anal/mean $prec/mean t30
-grd2nc ensemble/anal/sprd $prec/sprd t30
+# Process background ensemble mean and spread
+grd2nc ensemble/gues/mean $prec/gues_mean t30
+grd2nc ensemble/gues/sprd $prec/gues_sprd t30
 
-# Make the NetCDF file CF Conventions conforming
-fixmetadata $prec/mean
-fixmetadata $prec/sprd
+# Process analysis ensemble mean and spread
+grd2nc ensemble/anal/mean $prec/anal_mean t30
+grd2nc ensemble/anal/sprd $prec/anal_sprd t30
+
+# Make the NetCDF files CF Conventions conforming
+fixmetadata $prec/gues_mean
+fixmetadata $prec/gues_sprd
+fixmetadata $prec/anal_mean
+fixmetadata $prec/anal_sprd
+
+# Process ensemble data if necessary
+if test $save_ens -eq 1
+then
+    for MEM in $(seq -f "%03g" 1 $n_ens)
+    do
+        mkdir -p $SPEEDY/experiments/$exp_name/$prec/$MEM
+        grd2nc ensemble/anal/$MEM $prec/$MEM/anal t30
+        grd2nc ensemble/gues/$MEM $prec/$MEM/gues t30
+        fixmetadata $prec/$MEM/gues
+        fixmetadata $prec/$MEM/anal
+    done
+fi
